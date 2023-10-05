f2dc8aeccaa61a56b352713eda61884972fd3c91
emsiti2424 <sebastian.doana@softtekprojects.com>
2023-10-05 11:33:27 +0200

    mejoras

--- src/background.js
+++ src/background.js
@@ -4,25 +4,25 @@ import * as THREE from 'three';
 class Particle {
   constructor() {
     this.position = new THREE.Vector3(
-      Math.random() * 200 - 50,
-      Math.random() * 200 - 50,
-      Math.random() * 200 - 50
+      Math.random() * 200 - 100,
+      Math.random() * 200 - 100,
+      Math.random() * 200 - 100
     );
 
     this.velocity = new THREE.Vector3(
-      (Math.random() - 0.5) * 2,
-      (Math.random() - 0.5) * 2,
-      (Math.random() - 0.5) * 2
+      (Math.random() - 0.5) * 0.5,  // Making the movement slower
+      (Math.random() - 0.5) * 0.5,
+      (Math.random() - 0.5) * 0.5
     );
   }
 
   update() {
     this.position.add(this.velocity);
-    if (this.position.x > 50 || this.position.x < -50)
+    if (this.position.x > 100 || this.position.x < -100)
       this.velocity.x = -this.velocity.x;
-    if (this.position.y > 50 || this.position.y < -50)
+    if (this.position.y > 100 || this.position.y < -100)
       this.velocity.y = -this.velocity.y;
-    if (this.position.z > 50 || this.position.z < -50)
+    if (this.position.z > 100 || this.position.z < -100)
       this.velocity.z = -this.velocity.z;
   }
 }
@@ -63,72 +63,48 @@ export class MyThreeBg extends LitElement {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.shadowRoot.appendChild(this.renderer.domElement);
     this.scene.background = new THREE.Color(0x540d6e);
-    this.camera.position.z = 50;
+    this.camera.position.z = 100;
 
     const textureLoader = new THREE.TextureLoader();
     const particleTexture = textureLoader.load('../assets/open-wc-logo.svg');
+    const spriteMaterial = new THREE.SpriteMaterial({ map: particleTexture, blending: THREE.AdditiveBlending, transparent: true });
 
-    const particleMaterial = new THREE.PointsMaterial({
-      size: 5,
-      map: particleTexture,
-      transparent: true,
-      blending: THREE.AdditiveBlending,
-      depthWrite: false,
-      sizeAttenuation: true,
-    });
-
-    const particleGeom = new THREE.BufferGeometry();
-    const particleVertices = [];
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 20; i++) {
       this.spawnParticle();
-      particleVertices.push(
-        this.particles[i].position.x,
-        this.particles[i].position.y,
-        this.particles[i].position.z
-      );
     }
-    particleGeom.setAttribute(
-      'position',
-      new THREE.Float32BufferAttribute(particleVertices, 3)
-    );
-    const particleSystem = new THREE.Points(particleGeom, particleMaterial);
-    this.scene.add(particleSystem);
-
-    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
 
     const animate = () => {
-      requestAnimationFrame(animate);
-      this.particles.forEach(particle => {
-        particle.update();
-      });
+        requestAnimationFrame(animate);
+        this.particles.forEach(particle => {
+            particle.update();
+        });
 
-      this.scene.children.forEach(child => {
-        if (child instanceof THREE.Line) {
-          this.scene.remove(child);
-        }
-      });
+        // Remove old sprites (connections) from the scene
+        this.scene.children.forEach(child => {
+            if (child instanceof THREE.Sprite) {
+                this.scene.remove(child);
+            }
+        });
 
-      for (let i = 0; i < this.particles.length; i++) {
-        for (let j = i + 1; j < this.particles.length; j++) {
-          const distance = this.particles[i].position.distanceTo(
-            this.particles[j].position
-          );
-          if (distance < 40) {
-            const geometry = new THREE.BufferGeometry().setFromPoints([
-              this.particles[i].position,
-              this.particles[j].position,
-            ]);
-            const alpha = 2.0 - distance / 15.0;
-            lineMaterial.color.setHSL(0.5, 1.0, 0.5 + alpha / 2.0);
-            const line = new THREE.Line(geometry, lineMaterial);
-            this.scene.add(line);
-          }
+        for (let i = 0; i < this.particles.length; i++) {
+            for (let j = i + 1; j < this.particles.length; j++) {
+                const distance = this.particles[i].position.distanceTo(this.particles[j].position);
+                if (distance < 40) {
+                    const midpoint = new THREE.Vector3();
+                    midpoint.addVectors(this.particles[i].position, this.particles[j].position).multiplyScalar(0.5);
+
+                    const sprite = new THREE.Sprite(spriteMaterial);
+                    sprite.position.set(midpoint.x, midpoint.y, midpoint.z);
+                    sprite.scale.set(distance, distance, 1); // scale it based on distance, so it fills the gap between particles
+                    this.scene.add(sprite);
+                }
+            }
         }
-      }
-      this.renderer.render(this.scene, this.camera);
-      this.camera.position.x = 50 * Math.sin(Date.now() * 0.0001);
-      this.camera.position.z = 50 * Math.cos(Date.now() * 0.0001);
-      this.camera.lookAt(0, 0, 0);
+
+        this.renderer.render(this.scene, this.camera);
+        this.camera.position.x = 100 * Math.sin(Date.now() * 0.00005); // Making camera movement slower and more attractive
+        this.camera.position.z = 100 * Math.cos(Date.now() * 0.00005);
+        this.camera.lookAt(0, 0, 0);
     };
 
     animate();
